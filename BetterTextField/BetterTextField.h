591f128f328cb031de54315620bd7dd918f27d2c
Hua Duong Nguyen <HuaDuong.Nguyen@mediaman.de>
2016-06-10 23:16:18 +0200

    initial commit

---
+++ BetterTextField/BetterTextField.h
@@ -0,0 +1,19 @@
+//
+//  BetterTextField.h
+//  BetterTextField
+//
+//  Created by Hua Duong Nguyen on 10/06/16.
+//  Copyright © 2016 fixbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetterTextField.
+FOUNDATION_EXPORT double BetterTextFieldVersionNumber;
+
+//! Project version string for BetterTextField.
+FOUNDATION_EXPORT const unsigned char BetterTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterTextField/PublicHeader.h>
+
+
